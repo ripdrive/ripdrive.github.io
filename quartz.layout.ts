80119900d33acd53e0bce257eb5a2d09445e55b1
threehymns <threehymns1@gmail.com>
2024-08-03 04:37:52 -0400

    Hide Discord Link

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/ripdrive/ripdrive",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      // "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
 }
