db514fd4ae441f203069ae9e8e9b2ee5591d63c5
threehymns <threehymns1@gmail.com>
2024-08-04 22:44:24 -0400

    Add a page title suffix config option

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,6 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "The Rip Drive Project",
+    titleSuffix: " | The Rip Drive Project",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
